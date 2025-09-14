f325f1028ca12929b54b63705a96346facefc7d9
Jack Gardner <90489100+jacknelsongardner@users.noreply.github.com>
2025-09-14 14:46:56 -0700

    Create Clock.js
    Created clock.js for clock management

---
+++ supabase/functions/utils/Clock.js
@@ -0,0 +1,33 @@
+
+const MAX_WALL_TIME = 5000;
+const MAX_CPU_TIME = 1000;
+
+var startWallTime = Date.now();
+
+
+var startCpuTime = 0;
+var current_cpu_time = 0;
+
+function wallStart() {
+   startWallTime = Date.now();
+}
+
+function wallStop() {
+    if ((Date.now() - startWallTime) > MAX_CPU_TIME) {
+      return true;
+    } else {return false; }
+}
+
+function cpuStart() {
+    startCpuTime = Date.now()
+}
+
+function cpuStop() {
+    var time_elapsed = Date.now() - startCpuTime; 
+    current_cpu_time = current_cpu_time + time_elapsed;
+    if (current_cpu_time > MAX_CPU_TIME) {
+      return true;
+    } else {return false; }
+}
+
+export {cpuStart, cpuStop, wallStart, wallStop}
