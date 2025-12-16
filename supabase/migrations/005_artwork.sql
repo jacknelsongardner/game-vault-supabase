1e0fcdb907156e1d1c6d364ba96a6151453ee508
Jack Gardner <90489100+jacknelsongardner@users.noreply.github.com>
2025-12-15 17:48:05 -0800

    Refactored artwork SQL

--- supabase/migrations/005_artwork.sql
+++ supabase/migrations/005_artwork.sql
@@ -6,24 +6,48 @@ CREATE TABLE artwork_enum (
 
 CREATE TABLE artwork (
     id SERIAL PRIMARY KEY,
-    url TEXT,
-    data JSONB
+    url TEXT--,
+    --data JSONB
 );
 
 CREATE TABLE cover (
     id SERIAL PRIMARY KEY,
-    url TEXT,
-    data JSONB
+    url TEXT--,
+    --data JSONB
 );
 
 CREATE TABLE screenshot (
     id SERIAL PRIMARY KEY,
-    url TEXT,
-    data JSONB
+    url TEXT--,
+    --data JSONB
 );
 
 CREATE TABLE game_video (
     id SERIAL PRIMARY KEY,
-    url TEXT,
-    data JSONB
+    url TEXT--,
+    --data JSONB
+);
+
+CREATE TABLE artwork_of (
+    artwork_id INTEGER REFERENCES artwork(id),
+    game_id INTEGER REFERENCES game(id),
+    PRIMARY KEY (artwork_id, game_id)
+);
+
+CREATE TABLE cover_of (
+    cover_id INTEGER REFERENCES cover(id),
+    game_id INTEGER REFERENCES game(id),
+    PRIMARY KEY (cover_id, game_id)
+);
+
+CREATE TABLE screenshot_of (
+    screenshot_id INTEGER REFERENCES screenshot(id),
+    game_id INTEGER REFERENCES game(id),
+    PRIMARY KEY (screenshot_id, game_id)
+);
+
+CREATE TABLE game_video_of (
+    game_video_id INTEGER REFERENCES game_video(id),
+    game_id INTEGER REFERENCES game(id),
+    PRIMARY KEY (game_video_id, game_id)
 );
