36b59d1cae3c2b73c4b343377e3268ad4d7a7a83
Jack Gardner <90489100+jacknelsongardner@users.noreply.github.com>
2026-02-05 22:56:16 -0800

    Added platform aspects

--- supabase/migrations/001_platform.sql
+++ supabase/migrations/001_platform.sql
@@ -1,12 +1,37 @@
+CREATE TABLE platform_logo (
+    id INTEGER PRIMARY KEY,
+    url TEXT
+);
+
+CREATE TABLE platform_category (
+    id INTEGER PRIMARY KEY,
+    name TEXT--,
+    --data JSONB
+);
+
+CREATE TABLE platform_type (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE platform_family (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
 
 -- platform
 CREATE TABLE platform (
     id INTEGER PRIMARY KEY,
     name TEXT,
-    search_name TEXT--,
-    --alternate_name TEXT,
-    --abbreviation TEXT, 
-    --data JSONB
+    url TEXT,
+    summary TEXT,
+    search_name TEXT,
+    platform_family INTEGER REFERENCES platform_family(id),
+    platform_logo INTEGER REFERENCES platform_logo(id),
+    platform_type INTEGER REFERENCES platform_type(id),
+    generation INTEGER,
+    alternate_name TEXT,
+    abbreviation TEXT
 );
 
 CREATE TABLE platform_enum (
@@ -22,19 +47,3 @@ INSERT INTO platform_enum (id, name) VALUES
     (5, 'portable_console'),
     (6, 'computer');
 
-CREATE TABLE platform_logo (
-    id INTEGER PRIMARY KEY--,
-    -- alpha_channel BOOLEAN,
-    -- animated BOOLEAN,
-    -- height INTEGER,
-    -- image_id TEXT,
-    -- url TEXT,
-    -- width INTEGER,
-    -- checksum TEXT
-);
-
-CREATE TABLE platform_category (
-    id INTEGER PRIMARY KEY,
-    name TEXT--,
-    --data JSONB
-);
